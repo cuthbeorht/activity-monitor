2e5a2093f42170f6e686b0f667fe34d852cc299e
David Sciacchettano <david.sciacchettano@gmail.com>
2025-02-10 19:59:50 -0500

    Get a token - simple

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	token := getToken(os.Args)
+	fmt.Println("Status: ", token)
+}
+
+func getToken(arguments []string) string {
+	if len(arguments) != 2 {
+		log.Fatal("Invalid number of argumets. Expected 1, got", len(arguments)-1)
+	}
+
+	return arguments[1]
 }
